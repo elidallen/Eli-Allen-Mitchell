70a299eff58fda2471f48e36227736810c04e2c7
elidallen <134967288+elidallen@users.noreply.github.com>
2023-07-16 16:21:44 -0400

    initial commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassain from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+
+export { google, slack, atlassain, dropbox, shopify };
